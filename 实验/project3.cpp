76b9b90b9a76fca1ec8b7805d844fe47208d6946
Haiyun1016 <15991107@qq.com>
2021-11-28 22:06:05 +0800

    abc

---
+++ 实验/project3.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+#include <string>
+
+class Account
+{
+	public:
+		Account(int,string);
+		void display_1();
+		
+	protected:
+		int account;   //’À∫≈ 
+		string name;   //–’√˚ 
+		 
+		 
+};
+Account::Account(int acc,string nam)
+{
+	account = acc;
+	name = nam;
+}
+
+void Account::display_1()
+{
+	cout << setw(6) << setfill('0') << account << "\t\t" << name;  
+}
+
+class SavingsAccount : public Account
+{
+	public:
+		SavingsAccount(int acc,string nam,float bal):Account(acc,nam)
+		{
+			balance = bal;
+		}
+		void display_2();
+	protected:
+		float balance;
+};
+
+void SavingsAccount::display_2()
+{
+	display_1();
+	cout << "\t" << balance << endl;
+}
+
+class CheckAccount : public Account
+{
+	public:
+		CheckAccount(int acc,string nam,int i):Account(acc,nam)
+		{
+			id = i;
+		}
+		void display_3();
+	protected:
+		int id;
+};
+
+void CheckAccount::display_3()
+{	
+	display_1();
+	cout << "\t" << id << endl;	
+}
+
+int main()
+{
+	cout <<"Account£∫\n";
+	Account ac1(1231,"Zhangsan");
+	cout << "’À∫≈\t\t" << "–’√˚\n" << endl;
+	ac1.display_1();
+	cout << "\n";
+	
+	cout << "\n" << "SavingsAccount£∫\n";
+	SavingsAccount sa1(1231,"Zhangsan",1829.23);
+	cout << "’À∫≈\t\t" << "–’√˚\t\t" << "”‡∂Ó\n" << endl;
+	sa1.display_2();
+	
+	cout << "\n" << "CheckAccount£∫\n"; 
+	CheckAccount ch1(1231,"Zhangsan",123456);
+	cout << "’À∫≈\t\t" << "–’√˚\t\t" << "…Ì∑›÷§∫≈¬Î\n" << endl;
+	ch1.display_3();
+	
+	int id;        //…Ì∑›÷§∫≈¬Î
+	return 0;
+}
